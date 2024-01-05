3dcf614e942d2935e67a145a0d94399c717acd8d
devyumi <yyumiwing@naver.com>
2024-01-05 16:02:56 +0900

    build: build 파일 수정

---
+++ src/main/java/com/example/simplebest/SimplebestApplication.java
@@ -0,0 +1,12 @@
+package com.example.simplebest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimplebestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimplebestApplication.class, args);
+	}
+}
